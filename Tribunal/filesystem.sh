41a81a42a06d14b982880f881f11d986fc0566d4
Ernani Kern <ernani.kern@gmail.com>
2020-07-28 16:02:44 -0300

    filesystem.sh

--- Tribunal/filesystem.sh
+++ Tribunal/filesystem.sh
@@ -6,3 +6,7 @@ touch filesystem"$data".csv
 
 find /home/g1* -printf "%f;%Ab%AY;%k \n" >> filesystem"$data".csv
 
+# %f - nome do arquivo sem o caminho completo
+# %Ab%AY - data de último acesso do arquivo (%A), no formato mes(b) e ano(Y)
+# %k - espaço usado pelo arquivo em blocos de 1k
+# \n - nova linha
