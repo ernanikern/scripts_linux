7078f3daaa188416a408a1aaa6dd2a06a71fa5b4
Ernani Kern <ernani.kern@gmail.com>
2020-07-29 17:45:09 -0300

    import_csv.sh

--- Tribunal/import_csv.sh
+++ Tribunal/import_csv.sh
@@ -3,6 +3,6 @@
 cd /home/ernani/Dropbox/linux/scripts/Tribunal/fs_interior
 
 for file in *.csv
-	do
-	mysql -e "USE filesystemsTRT LOAD DATA LOCAL INFILE '"$file"' INTO TABLE results FIELDS TERMINATED BY '/' ENCLOSED BY '"' LINES TERMINATED BY '\n';"
+do
+	mysql -uernani -pkrq6XY -DfilesystemsTRT -e "LOAD DATA INFILE $file INTO TABLE results LINES TERMINATED BY '\n';"
 done
