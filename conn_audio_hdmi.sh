a6c98e77e04cbf0271b22f70ecafd7016331166d
Ernani Kern <ernani.kern@gmail.com>
2020-08-06 18:13:58 -0300

    conn_audio_*

--- conn_audio_hdmi.sh
+++ conn_audio_hdmi.sh
@@ -2,14 +2,17 @@
 
 # LISTA OS DISPOSITIVOS DE SAÍDA, MANTÉM APENAS O HDMI E BLUETOOTH, E DETECTA QUAL ESTÁ SEM USO (SUSPENDED):
 
-ativar=`pactl list short sinks | grep hdmi | grep SUSPENDED | awk '{print $1, $2}'`
+ativar=`pactl list short sinks | grep hdmi | awk '{print $1, $2}'`
 echo "ativar = $ativar"
 if [ "$ativar" == "" ]; then
   echo "Saída já selecionada."
 else
-  novo=`echo $ativar | awk '{print $2}'`
-  echo "novo = $novo"
-  pacmd set-default-sink "$novo"
+  id=`echo $ativar | awk '{print $2}'`
+  nome=`echo $ativar | awk '{print $2}'`
+  echo "id = $id"
+  echo "nome = $nome"
+  pacmd set-default-sink "$id"
+  pacmd set-default-sink "$nome"
   pactl list short sinks
 fi
 # VERIFICA SE O ÚNICO DISPOSITIVO JÁ ESTÁ EM USO (CAUSA $ativar = ""):
--- conn_audio_logitechX300.sh
+++ conn_audio_logitechX300.sh
@@ -2,14 +2,17 @@
 
 # LISTA OS DISPOSITIVOS DE SAÍDA, MANTÉM APENAS O HDMI E BLUETOOTH, E DETECTA QUAL ESTÁ SEM USO (SUSPENDED):
 
-ativar=`pactl list short sinks | grep "88_C6_26_11_65_26" | grep SUSPENDED | awk '{print $1, $2}'`
+ativar=`pactl list short sinks | grep "88_C6_26_11_65_26" | awk '{print $1, $2}'`
 echo "ativar = $ativar"
 if [ "$ativar" == "" ]; then
-  echo "Saída já selecionada."
+  echo "Nada retornado"
 else
-  novo=`echo $ativar | awk '{print $1}'`
-  echo "novo = $novo"
-  pacmd set-default-sink "$novo"
+  id=`echo $ativar | awk '{print $1}'`
+  nome=`echo $ativar | awk '{print $2}'`
+  echo "id = $id"
+  echo "nome = $nome"
+  pacmd set-default-sink "$id"
+  pacmd set-default-sink "$nome"
   pactl list short sinks
 fi
 # VERIFICA SE O ÚNICO DISPOSITIVO JÁ ESTÁ EM USO (CAUSA $ativar = ""):
