6202b48602af1b92801f8670b92b04fbfae3194a
Ernani Kern <ernani.kern@gmail.com>
2020-08-07 12:10:53 -0300

    filesystem.sh

--- Tribunal/filesystem.sh
+++ Tribunal/filesystem.sh
@@ -5,9 +5,10 @@ file="filesystem_$host.csv"
 rm "$file"
 
 #find /home/g1*/textos/ -printf "%f;%Ab%AY;%k \n" >> "$file"
-find /home/g1*/textos/ -printf "%f/%AY-%Am-%Ad/%k \n" | grep -v "textos//" >> "$file"
+find /home/g1*/textos/ -printf "%f/%AY-%Am-%Ad/%s \n" | grep -v "textos//" >> "$file"
 
 # %f - nome do arquivo sem o caminho completo
 # %AY-%Am-%Ad - data de último acesso do arquivo (%A), no formato YYYY-mm-dd
+# %s - tamanho do arquivo
 # %k - espaço usado pelo arquivo em blocos de 1k
 # \n - nova linha
