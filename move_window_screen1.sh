11d82b7379c088b3b61bf85089848476448ea469
Ernani Kern <ernani.kern@gmail.com>
2020-10-08 22:26:56 -0300

    move_window_screen1.sh' !

--- move_window_screen1.sh
+++ move_window_screen1.sh
@@ -1,3 +1,12 @@
 #!/bin/bash
 
-xfce4-terminal -e 'wmctrl -l'
+#xfce4-terminal -e 'wmctrl -l'
+#
+if [ "$1" == "" ]; then
+	echo "Informe parte (única) do nome da janela a ser movida (case insensitive)."
+	echo "Execute 'wmctrl -lp para listar as janelas.'"
+else
+	echo "1 = $1"
+  wmctrl -r "$1" -e 0,0,1111,-1,-1
+	wmctrl -a "$1"
+fi
