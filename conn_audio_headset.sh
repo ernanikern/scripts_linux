de4c0ac0152087b8cfa484a15636c9a8091b4679
Ernani Kern <ernani.kern@gmail.com>
2021-05-17 17:40:28 -0300

    conn_audio_headset.sh

---
+++ conn_audio_headset.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+
+# LISTA OS DISPOSITIVOS DE SAÍDA, MANTÉM APENAS O HEADSET USB, E DETECTA QUAL ESTÁ SEM USO (SUSPENDED):
+
+ativar=`pactl list short sinks | grep usb | awk '{print $1, $2}'`
+echo "ativar = $ativar"
+if [ "$ativar" == "" ]; then
+  echo "Saída já selecionada."
+else
+  id=`echo $ativar | awk '{print $1}'`
+  nome=`echo $ativar | awk '{print $2}'`
+  echo "id = $id"
+  echo "nome = $nome"
+  pacmd set-default-sink "$id"
+  pacmd set-default-sink "$nome"
+  pactl list short sinks
+fi
+# VERIFICA SE O ÚNICO DISPOSITIVO JÁ ESTÁ EM USO (CAUSA $ativar = ""):
+
+# if [ "$ativar" != "" ];then
+#   echo "novo = $novo"
+#   pacmd set-default-sink $novo
+# else 
+#   echo "Não há outro dispositivo conectado."
+# fi
